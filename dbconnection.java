97aa7316adaadc80f390717e22e76b24a4f6e703
santoshkamthe <santoshkamthe9121@gmail.com>
2023-07-12 04:42:59 +0000

    first commit related to jdbc

---
+++ dbconnection.java
@@ -0,0 +1,22 @@
+package com.velocity.Demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JdbcSample {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+//register driver class
+		Class.forName("com.mysql.jdbc.Driver");
+//create connection
+		String url = "jdbc:mysql://localhost:3306/empdb?characterEncoding=utf8";
+		Connection connection = DriverManager.getConnection(url, "root", "root");
+//create statement
+		Statement statement = connection.createStatement();
+//execute statement
+		String sqlquery = "insert into sample(name,email)values('pranita','pranitakamthe95@outlook.com')";
+		Boolean isInserted = statement.execute(sqlquery);
+		System.out.println(isInserted);
+	}
+}
